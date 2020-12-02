94451b2ff5989745b0f0992f08910f1f989ad053
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2020-12-03 00:00:50 +0300

    Add files via upload

---
+++ SorticE().py
@@ -0,0 +1,101 @@
+global a
+global b
+
+
+def sa():
+    if a == list():
+        pass
+    elif a[0] == a[-1]:
+        pass
+    else:
+        a[0], a[1] = a[1], a[0]
+
+
+def sb():
+    if b == list():
+        pass
+    elif b[0] == b[-1]:
+        pass
+    else:
+        b[0], b[1] = b[1], b[0]
+
+
+def function():
+    pass
+
+
+def ss():
+    sa()
+    sb()
+
+
+def pa():
+    global a, b
+    if b == list():
+        pass
+    else:
+        a = [b[0]] + a
+    b = b[1:]
+
+
+def pb():
+    global a, b
+    if a == list():
+        pass
+    else:
+        b = [a[0]] + b
+    a = a[1:]
+
+
+def ra():
+    global a
+    kol = 0
+    for i in a:
+        kol += 1
+    res = a[-1]
+    for i in range(kol):
+        a[i - 1] = a[i]
+    a[-2] = res
+
+
+def rb():
+    global b
+    kol = 0
+    for i in b:
+        kol += 1
+    res = b[-1]
+    for i in range(kol):
+        b[i - 1] = b[i]
+    b[-2] = res
+
+
+def rr():
+    ra()
+    rb()
+
+
+def rra():
+    global a
+    kol = 0
+    for i in a:
+        kol += 1
+    res = a[0]
+    for i in range(kol):
+        a[-i] = a[-i - 1]
+    a[1] = res
+
+
+def rrb():
+    global b
+    kol = 0
+    for i in b:
+        kol += 1
+    res = b[0]
+    for i in range(kol):
+        b[-i] = b[-i - 1]
+    b[1] = res
+
+
+def rrr():
+    rra()
+    rrb()
