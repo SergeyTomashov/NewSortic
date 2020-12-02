8d12c7c12ea4c1f98bb7f0614a1f83b9fa6437b4
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2020-12-02 21:34:21 +0300

    Update ss().py

--- ss().py
+++ ss().py
@@ -1,20 +1,4 @@
-global a, b
-def sa(a):
-    if a == list():
-        pass
-    elif a[0] == a[-1]:
-        pass
-    else:
-        a[0], a[1] = a[1], a[0]
-
-def sb(b):
-    if b == list():
-        pass
-    elif b[0] == b[-1]:
-        pass
-    else:
-        b[0], b[1] = b[1], b[0]
-
 def ss():
-    sa(a)
-    sb(b)
+    global a, b
+    sa()
+    sb()
