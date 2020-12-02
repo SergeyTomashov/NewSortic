4388c8250b80fa867a092cb4a05012db146cde3f
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2020-12-02 20:38:51 +0300

    Update sb.py

--- sb.py
+++ sb.py
@@ -1,5 +1,8 @@
 def sb(b):
-    if b[0] == '' or b[1] == '':
+    if b == list():
         pass
-    b[0], b[1] = b[1], b[0]
+    elif b[0] == b[-1]:
+        pass
+    else:
+        b[0], b[1] = b[1], b[0]
     return b
